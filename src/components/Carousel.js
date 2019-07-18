6918b9da69c2600b93427136b881714c0328690f
DarekR88 <dradmclad@gmail.com>
2019-07-18 15:03:36 -0500

    react gesture

---
+++ src/components/Carousel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Gallery, GalleryImage } from "react-gesture-gallery";
+
+class Carousel extends Component {
+    
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
